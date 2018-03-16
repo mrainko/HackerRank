fde00e852fbc30c4b13b6624615e4c5ca43abc98
mrainko <37443481+mrainko@users.noreply.github.com>
2018-03-16 14:36:18 +0100

    Update BirthdayChocolate.cpp

--- Algoritmi/Algoritmi/BirthdayChocolate.cpp
+++ Algoritmi/Algoritmi/BirthdayChocolate.cpp
@@ -8,5 +8,40 @@ using namespace std;
 
 void BirthdayChocolate()
 {
-  
+  int n;
+	std::cin >> n;
+
+	std::vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> v[i];
+	}
+
+	int d, m;
+	std::cin >> d >> m;
+
+	int o = 0;
+	int sum = 0;
+	int counter = 0;
+
+	if (v.size() == 1)
+	{
+		if (v[0] == d)
+			counter++;
+	}
+	else
+	{
+		while (o <= (v.size() - m))
+		{
+			for (int i = o; i < m + o; i++)
+				sum += v[i];
+
+			if (sum == d)
+				counter++;
+            
+			sum = 0;
+			o++;
+		}
+	}
+	std::cout << counter << std::endl;
 }
