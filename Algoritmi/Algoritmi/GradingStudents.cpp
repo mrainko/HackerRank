d5efadfb1a08e65bee9c5d79424d6bfc59b11ddd
mrainko <37443481+mrainko@users.noreply.github.com>
2018-03-16 14:22:20 +0100

    Create GradingStudents.cpp

---
+++ Algoritmi/Algoritmi/GradingStudents.cpp
@@ -0,0 +1,40 @@
+void GradingStudents()
+{
+  int n;
+    std::vector<int> v;
+    int grade;
+    std::cin >> n;
+
+    if (n >= 1 && n <= 60)
+    {
+      for (int i = 0; i < n; i++)
+      {
+        std::cin >> grade;
+
+        if (grade >= 0 && grade <= 100)
+        {
+          if (grade >= 38)
+          {
+            int o = (grade / 5) + 1;
+            o = o * 5;
+
+            if ((o - grade) < 3)
+            {
+              v.push_back(o);
+            }
+            else
+              v.push_back(grade);
+          }
+          else
+            v.push_back(grade);
+        }
+        else
+          i--;
+      }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+      std::cout << v[i] << std::endl;
+    }
+  }
