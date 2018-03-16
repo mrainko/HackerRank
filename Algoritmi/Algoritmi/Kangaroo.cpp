da1d9601208df1baca874fd1249caf03cf7d063b
mrainko <37443481+mrainko@users.noreply.github.com>
2018-03-16 14:28:56 +0100

    Create Kangaroo.cpp

---
+++ Algoritmi/Algoritmi/Kangaroo.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <string>
+//#include "algoritmiHeader.h"
+
+using namespace std;
+
+void Kangaroo()
+{
+  int x1,v1, x2, v2;
+	std::cin >> x1 >> v1 >> x2 >> v2;
+
+	if (v1 <= v2)
+		std::cout << "NO" << std::endl;
+	else
+	{
+		if ((x2 - x1) % (v1 - v2) == 0)
+			std::cout << "YES" << std::endl;
+        else
+            std::cout << "NO" << std::endl;
+	}
+}
