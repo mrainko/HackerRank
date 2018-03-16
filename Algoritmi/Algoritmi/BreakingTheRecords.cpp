1a47dfca2b5b3b04fce0896adaf5260ddef9658b
mrainko <37443481+mrainko@users.noreply.github.com>
2018-03-16 14:32:58 +0100

    Update BreakingTheRecords.cpp

--- Algoritmi/Algoritmi/BreakingTheRecords.cpp
+++ Algoritmi/Algoritmi/BreakingTheRecords.cpp
@@ -1,1 +1,33 @@
+void BreakingTheRecords()
+{
+  int n;
+  int min, max;
+  int mn = 0, mx = 0;
+  std::cin >> n;
 
+  std::vector<int> v(n);
+
+  for (int i = 0; i < v.size(); i++)
+  {
+    std::cin >> v[i];
+  }
+
+  min = v[0];
+  max = v[0];
+
+  for (int i = 1; i < n; i++)
+  {
+    if (v[i] > max)
+    {
+      max = v[i];
+      mx++;
+    }
+    if (v[i] < min)
+    {
+      min = v[i];
+      mn++;
+    }
+  }
+
+  std::cout << mx << " " << mn << std::endl;
+}
